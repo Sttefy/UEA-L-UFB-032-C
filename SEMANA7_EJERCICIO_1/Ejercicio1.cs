54edd1f5cf5b08166b845b76aa40d515e7b93523
Sttefy <ms.gavilanezg@uea.edu.ec>
2025-01-26 20:21:32 -0500

    Creacion

---
+++ SEMANA7_EJERCICIO_1/Ejercicio1.cs
@@ -0,0 +1,51 @@
+﻿public class VerificadorBalance
+{
+    public static bool EstanBalanceados(string formula)
+    {
+        // Crea una pila para almacenar los caracteres
+        Stack<char> pilaSimbolos = new Stack<char>();
+
+        // Recorre cada caracter de la fórmula
+        foreach (char c in formula)
+        {
+            // Si el caracter es un paréntesis de apertura, corchete o llave, agregarlo a la pila
+            if (c == '(' || c == '[' || c == '{')
+            {
+                pilaSimbolos.Push(c);
+            }
+            // Si el caracter es un paréntesis de cierre, corchete o llave
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                // Si la pila se encuentra vacía, significa que no hay un par correspondiente
+                if (pilaSimbolos.Count == 0)
+                {
+                    return false;
+                }
+
+                // Saca el elemento que se encuentre en la cima de la pila
+                char top = pilaSimbolos.Pop();
+
+                // Verifica si el caracter corresponde a su par de apertura
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                {
+                    return false; // No corresponde a su par, es decir la fórmula no es balanceada
+                }
+            }
+        }
+
+        // Si la pila está vacía, la fórmula está balanceada
+        return pilaSimbolos.Count == 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Se agrega nuevo ejemplo de fórmula matemática
+        string formula = "[5+3*(2+1)] + {4*[(2+3)-(5+5)]}"; 
+        // Llama a la función para verificar si la fórmula está balanceada
+        bool balanceado = EstanBalanceados(formula);
+        // Imprime el resultado
+        Console.WriteLine($"Formula balanceada: {balanceado}");
+    }
+}
---
+++ SEMANA7_EJERCICIO_2/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Torres
+{
+    class Program
+    {
+        private const int NumDiscos = 6;
+
+        class Conjunto
+        {
+            public int[] Discos = new int[NumDiscos];
+            public int NumPlatos = 0;
+        }
+
+        class Torre
+        {
+            public Conjunto[] Pilas = new Conjunto[3];
+            public int NumPlatos;
+        }
+
+        static void TorreInit(Torre T, int num)
+        {
+            T.NumPlatos = num;
+            for (int i = 0; i < 3; i++) T.Pilas[i] = new Conjunto();
+            for (int i = num; i > 0; i--) T.Pilas[0].Discos[T.Pilas[0].NumPlatos++] = i;
+        }
+
+        static void TorreMoverDisco(Torre T, int Origen, int Destino)
+        {
+            int disco = T.Pilas[Origen].Discos[--T.Pilas[Origen].NumPlatos];
+            T.Pilas[Destino].Discos[T.Pilas[Destino].NumPlatos++] = disco;
+            TorreDraw(T);
+        }
+
+        static int CalcularAux(int Origen, int Destino)
+        {
+            if (Origen == 0 && Destino == 1) return 2;
+            if (Origen == 0 && Destino == 2) return 1;
+            if (Origen == 1 && Destino == 2) return 0;
+            if (Origen == 1 && Destino == 0) return 2;
+            if (Origen == 2 && Destino == 0) return 1;
+            if (Origen == 2 && Destino == 1) return 0;
+            return -1;
+        }
+
+        static void TorreMoverDisco(Torre T, int Origen, int Destino, int num)
+        {
+            int aux = CalcularAux(Origen, Destino);
+            if (num > 1) TorreMoverDisco(T, Origen, aux, num - 1);
+            TorreMoverDisco(T, Origen, Destino);
+            if (num > 1) TorreMoverDisco(T, aux, Destino, num -1);
+        }
+
+        static void TorreMover(Torre T, int Destino)
+        {
+            TorreMoverDisco(T, 0, Destino, T.NumPlatos);
+        }
+
+        static void TorreDraw(Torre T)
+        {
+            Console.WriteLine("===============================================================");
+            for (int p = 0; p < 3; p++)
+            {
+                Console.Write(p + ": ");
+                for (int i = 0; i < T.Pilas[p].NumPlatos; i++)
+                Console.Write(T.Pilas[p].Discos[i] + " ");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Torre T = new Torre();
+
+            TorreDraw(T);
+
+            TorreMover(T, 2);
+
+            Console.WriteLine("Listo");
+            Console.ReadKey();
+        }
+    }
+}
